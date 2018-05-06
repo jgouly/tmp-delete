bc424bdc0783e6e7ec5b1acd3b562ffc9ba72b90
Joey Gouly <joey.gouly@gmail.com>
2018-05-06 14:48:17 +0100

    cube: Add Cube::verify to check if a cube is solvable
    A new function Cube::new_unchecked will not call verify, for cases when
    an invalid cube is needed.
    
    Edge / Corner parity is checked by counting the number of inversions of
    the permutation.
    
    An inversion is where two of the elements are out of the natural order.
      i and j are elements.
      perm is a permutation.
      An inversion is if:
        i < j
        perm(i) > perm(j)

--- cube/src/lib.rs
+++ cube/src/lib.rs
@@ -1,5 +1,5 @@
 /// The corners on a 3x3x3 cube.
-#[derive(Debug, PartialEq)]
+#[derive(Clone, Copy, Debug, PartialEq, PartialOrd)]
 pub enum Corner {
   URF,
   UFL,
@@ -12,7 +12,7 @@ pub enum Corner {
 }
 
 /// The edges on a 3x3x3 cube.
-#[derive(Debug, PartialEq)]
+#[derive(Clone, Copy, Debug, PartialEq, PartialOrd)]
 pub enum Edge {
   UR,
   UF,
@@ -26,6 +26,16 @@ pub enum Edge {
   FL,
   BL,
   BR,
+}
+
+/// An error for the different invalid cube states.
+#[derive(Debug, PartialEq)]
+pub enum CubeStateErr {
+  ErrEO,
+  ErrCO,
+  ErrEP,
+  ErrCP,
+  ErrParity,
 }
 
 /// Number of corners on a 3x3x3 cube.
@@ -45,6 +55,19 @@ pub struct Cube {
 impl Cube {
   /// Creates a new `Cube` with the specified permutations and orientations.
   pub fn new(
+    cp: [Corner; NUM_CORNERS],
+    co: [u8; NUM_CORNERS],
+    ep: [Edge; NUM_EDGES],
+    eo: [u8; NUM_EDGES],
+  ) -> Cube {
+    let cube = Cube { cp, co, ep, eo };
+    cube.verify().unwrap();
+    cube
+  }
+
+  /// Creates a new `Cube` with the specified permutations and orientations.
+  /// This function does not check that the `Cube` is in a solvable state.
+  pub fn new_unchecked(
     cp: [Corner; NUM_CORNERS],
     co: [u8; NUM_CORNERS],
     ep: [Edge; NUM_EDGES],
@@ -83,4 +106,75 @@ impl Cube {
     let eo = [0; NUM_EDGES];
     Cube::new(cp, co, ep, eo)
   }
+
+  /// Verify that a `Cube` is in a solvable state.
+  pub fn verify(&self) -> Result<(), CubeStateErr> {
+    // Check that each edge is used only once.
+    let mut edges = 0u16;
+    for i in &self.ep {
+      edges |= 1 << (*i as u16);
+    }
+    if edges != 0b111111111111 {
+      return Err(CubeStateErr::ErrEP);
+    }
+
+    // Check that each edge orientation is 0 or 1.
+    if !self.eo.iter().all(|&eo| eo <= 1) {
+      return Err(CubeStateErr::ErrEO);
+    }
+
+    // Check that the total edge orientation is a multiple of 2.
+    let eo: u8 = self.eo.iter().sum();
+    if eo % 2 != 0 {
+      return Err(CubeStateErr::ErrEO);
+    }
+
+    // Check that each corner is used only once.
+    let mut corners = 0u8;
+    for i in &self.cp {
+      corners |= 1 << (*i as u8);
+    }
+    if corners != 0b11111111 {
+      return Err(CubeStateErr::ErrCP);
+    }
+
+    // Check that each edge orientation is 0, 1 or 2.
+    if !self.co.iter().all(|&co| co <= 2) {
+      return Err(CubeStateErr::ErrCO);
+    }
+
+    // Check that the total corner orientation is a multiple of 3.
+    let co: u8 = self.co.iter().sum();
+    if co % 3 != 0 {
+      return Err(CubeStateErr::ErrCO);
+    }
+
+    // Check that corner parity and edge parity are equal.
+    if self.edge_parity() != self.corner_parity() {
+      return Err(CubeStateErr::ErrParity);
+    }
+    Ok(())
+  }
+
+  fn corner_parity(&self) -> bool {
+    num_inversions(&self.cp) % 2 != 0
+  }
+
+  fn edge_parity(&self) -> bool {
+    num_inversions(&self.ep) % 2 != 0
+  }
+}
+
+/// Count the number of inversions in a permutation.
+fn num_inversions<P: PartialOrd>(perm: &[P]) -> usize {
+  let mut num = 0;
+  for i in 0..perm.len() - 1 {
+    for j in i + 1..perm.len() {
+      assert!(i < j);
+      if perm[i] > perm[j] {
+        num += 1;
+      }
+    }
+  }
+  num
 }
--- cube/tests/cube.rs
+++ cube/tests/cube.rs
@@ -15,3 +15,76 @@ fn solved_cube() {
   );
   assert_eq!(solved, cube);
 }
+
+#[test]
+fn invalid_cube() {
+  let invalid_ep = Cube::new_unchecked(
+    [URF, UFL, ULB, UBR, DFR, DLF, DBL, DRB],
+    [0; NUM_CORNERS],
+    [UF; 12],
+    [0; NUM_EDGES],
+  );
+  assert_eq!(CubeStateErr::ErrEP, invalid_ep.verify().unwrap_err());
+
+  let invalid_eo = Cube::new_unchecked(
+    [URF, UFL, ULB, UBR, DFR, DLF, DBL, DRB],
+    [0; NUM_CORNERS],
+    [UR, UF, UL, UB, DR, DF, DL, DB, FR, FL, BL, BR],
+    [3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  );
+  assert_eq!(CubeStateErr::ErrEO, invalid_eo.verify().unwrap_err());
+
+  let invalid_eo = Cube::new_unchecked(
+    [URF, UFL, ULB, UBR, DFR, DLF, DBL, DRB],
+    [0; NUM_CORNERS],
+    [UR, UF, UL, UB, DR, DF, DL, DB, FR, FL, BL, BR],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  );
+  assert_eq!(CubeStateErr::ErrEO, invalid_eo.verify().unwrap_err());
+
+  let invalid_cp = Cube::new_unchecked(
+    [URF; 8],
+    [0; NUM_CORNERS],
+    [UR, UF, UL, UB, DR, DF, DL, DB, FR, FL, BL, BR],
+    [0; NUM_EDGES],
+  );
+  assert_eq!(CubeStateErr::ErrCP, invalid_cp.verify().unwrap_err());
+
+  let invalid_co = Cube::new_unchecked(
+    [URF, UFL, ULB, UBR, DFR, DLF, DBL, DRB],
+    [1, 0, 0, 0, 0, 0, 0, 0],
+    [UR, UF, UL, UB, DR, DF, DL, DB, FR, FL, BL, BR],
+    [0; NUM_EDGES],
+  );
+  assert_eq!(CubeStateErr::ErrCO, invalid_co.verify().unwrap_err());
+
+  let invalid_co = Cube::new_unchecked(
+    [URF, UFL, ULB, UBR, DFR, DLF, DBL, DRB],
+    [3, 0, 0, 0, 0, 0, 0, 0],
+    [UR, UF, UL, UB, DR, DF, DL, DB, FR, FL, BL, BR],
+    [0; NUM_EDGES],
+  );
+  assert_eq!(CubeStateErr::ErrCO, invalid_co.verify().unwrap_err());
+
+  let invalid_edge_parity = Cube::new_unchecked(
+    [URF, UFL, ULB, UBR, DFR, DLF, DBL, DRB],
+    [0; NUM_CORNERS],
+    [UF, UR, UL, UB, DR, DF, DL, DB, FR, FL, BL, BR],
+    [0; NUM_EDGES],
+  );
+  assert_eq!(
+    CubeStateErr::ErrParity,
+    invalid_edge_parity.verify().unwrap_err()
+  );
+
+  let invalid_corner_parity = Cube::new_unchecked(
+    [UFL, URF, ULB, UBR, DFR, DLF, DBL, DRB],
+    [0; NUM_CORNERS],
+    [UR, UF, UL, UB, DR, DF, DL, DB, FR, FL, BL, BR],
+    [0; NUM_EDGES],
+  );
+  assert_eq!(
+    CubeStateErr::ErrParity,
+    invalid_corner_parity.verify().unwrap_err()
+  );
+}
