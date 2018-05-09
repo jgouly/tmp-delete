8ed9078879fe30b91f43b127f7dab8a3480fbe8d
Joey Gouly <joey.gouly@gmail.com>
2018-05-09 23:44:38 +0100

    kociemba: Add G0 CO coordinate
    The G0 CO coordinate is a 7-digit number in base-3.
    
    Add exhaustive checks for the coordinates to ensure that setting a cube
    in a particular coordinate and then getting the coordinate from the
    cube returns the same value.

--- kociemba/src/lib.rs
+++ kociemba/src/lib.rs
@@ -2,4 +2,5 @@ extern crate cube;
 
 mod transition_table;
 
+pub use transition_table::get_co_transition_table;
 pub use transition_table::get_eo_transition_table;
--- kociemba/src/transition_table.rs
+++ kociemba/src/transition_table.rs
@@ -35,6 +35,32 @@ impl Coord for EOCoord {
   }
 }
 
+/// The G0 CO coordinate is 7 digit base-3 number where each digit corresponds
+/// to the orientation of the corner at that index. The 8th corner's orientation
+/// is calculated based on the first 7 corner orientations.
+struct COCoord;
+
+impl Coord for COCoord {
+  const NUM_ELEMS: usize = 2187; // 3 ^ 7
+
+  fn set_coord(cube: &mut Cube, co: usize) {
+    assert!(co < Self::NUM_ELEMS);
+    let mut co = co;
+    for i in (0..7).rev() {
+      cube.co[i] = (co % 3) as u8;
+      co /= 3;
+      cube.co[7] = ((cube.co[7] + 3) - cube.co[i]) % 3;
+    }
+    cube.verify().unwrap();
+  }
+
+  fn get_coord(cube: &Cube) -> usize {
+    cube.co[..7]
+      .iter()
+      .fold(0usize, |acc, &cur| (acc * 3) + (cur as usize))
+  }
+}
+
 fn init_transition_table<T: Coord>() -> Vec<[usize; 6]> {
   let mut v = vec![[0; 6]; T::NUM_ELEMS];
   let turn_counts = [1; 6];
@@ -53,6 +79,11 @@ fn init_transition_table<T: Coord>() -> Vec<[usize; 6]> {
   v
 }
 
+/// Get the G0 CO transition table.
+pub fn get_co_transition_table() -> Vec<[usize; 6]> {
+  init_transition_table::<COCoord>()
+}
+
 /// Get the G0 EO transition table.
 pub fn get_eo_transition_table() -> Vec<[usize; 6]> {
   init_transition_table::<EOCoord>()
@@ -64,6 +95,14 @@ mod tests {
   use cube::Corner::*;
   use cube::Edge::*;
   use cube::{NUM_CORNERS, NUM_EDGES};
+
+  fn exhaustive_coord_check<T: Coord>() {
+    for i in 0..T::NUM_ELEMS {
+      let mut c = Cube::solved();
+      T::set_coord(&mut c, i);
+      assert_eq!(i, T::get_coord(&c));
+    }
+  }
 
   #[test]
   fn eo_coord() {
@@ -91,5 +130,43 @@ mod tests {
     let c = Cube::solved();
     let c = c.apply_move(Move(Face::U, 3));
     assert_eq!(0, eo[EOCoord::get_coord(&c)][Face::U as usize]);
+  }
+
+  #[test]
+  fn eo_coord_exhaustive() {
+    exhaustive_coord_check::<EOCoord>();
+  }
+
+  #[test]
+  fn co_coord() {
+    let c = Cube::solved();
+    assert_eq!(0, COCoord::get_coord(&c));
+
+    for i in 1..4 {
+      let c = c.apply_move(Move(Face::U, i));
+      assert_eq!(0, COCoord::get_coord(&c));
+    }
+
+    let c = Cube::new(
+      [URF, UFL, ULB, UBR, DFR, DLF, DBL, DRB],
+      [2, 2, 2, 2, 2, 2, 2, 1],
+      [UR, UF, UL, UB, DR, DF, DL, DB, FR, FL, BL, BR],
+      [0; NUM_EDGES],
+    );
+    assert_eq!(COCoord::NUM_ELEMS - 1, COCoord::get_coord(&c));
+  }
+
+  #[test]
+  fn co_transition() {
+    let co = get_co_transition_table();
+
+    let c = Cube::solved();
+    let c = c.apply_move(Move(Face::F, 3));
+    assert_eq!(0, co[COCoord::get_coord(&c)][Face::F as usize]);
+  }
+
+  #[test]
+  fn co_coord_exhaustive() {
+    exhaustive_coord_check::<COCoord>();
   }
 }
