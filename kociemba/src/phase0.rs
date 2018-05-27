2013e92272b255ab5c13abe8e1ce3a152fc755cd
Joey Gouly <joey.gouly@gmail.com>
2018-05-27 23:49:29 +0100

    kociemba: Recurse inside phase0
    This adds Phase0Tables, which uses the transition tables to move from
    one coordinate to another.

--- kociemba/src/phase0.rs
+++ kociemba/src/phase0.rs
@@ -1,4 +1,4 @@
-use cube::Cube;
+use cube::{Cube, Face};
 use transition_table::COCoord;
 use transition_table::Coord;
 use transition_table::EOCoord;
@@ -27,27 +27,75 @@ impl From<Cube> for Phase0Coord {
   }
 }
 
+pub struct Phase0Tables<'a> {
+  eo_t: &'a [[usize; 6]],
+  co_t: &'a [[usize; 6]],
+  ud1_t: &'a [[usize; 6]],
+}
+
+impl<'a> Phase0Tables<'a> {
+  fn transition(&self, coord: Phase0Coord, face: Face) -> Phase0Coord {
+    let eo = self.eo_t[coord.eo][usize::from(face)];
+    let co = self.co_t[coord.co][usize::from(face)];
+    let ud1 = self.ud1_t[coord.ud1][usize::from(face)];
+    Phase0Coord { eo, co, ud1 }
+  }
+}
+
 /// Phase 0: Reduce a cube from G0 to G1.
-pub fn phase0(coord: Phase0Coord) -> bool {
-  coord.is_solved()
+pub fn phase0(
+  coord: Phase0Coord,
+  depth_remaining: usize,
+  tables: &Phase0Tables,
+) -> bool {
+  if depth_remaining == 0 {
+    return coord.is_solved();
+  }
+
+  for &f in &[Face::U, Face::D, Face::F, Face::B, Face::R, Face::L] {
+    let next = tables.transition(coord, f);
+    if phase0(next, depth_remaining - 1, tables) {
+      return true;
+    }
+  }
+  false
 }
 
 #[cfg(test)]
 mod tests {
   use super::*;
-  use cube::{Face, Move};
+  use cube::Move;
+  use transition_table::*;
 
   #[test]
   fn basic() {
+    let co_t = get_co_transition_table();
+    let eo_t = get_eo_transition_table();
+    let ud1_t = get_ud1_transition_table();
+
+    let tables = Phase0Tables {
+      co_t: &co_t,
+      eo_t: &eo_t,
+      ud1_t: &ud1_t,
+    };
+
     let c = Cube::solved();
-    assert!(phase0(c.into()));
+    assert!(phase0(c.into(), 0, &tables));
 
     let c = Cube::solved();
     let c = c.apply_move(Move(Face::U, 1));
-    assert!(phase0(c.into()));
+    assert!(phase0(c.into(), 0, &tables));
 
     let c = Cube::solved();
     let c = c.apply_move(Move(Face::F, 1));
-    assert!(!phase0(c.into()));
+    assert!(!phase0(c.into(), 0, &tables));
+    assert!(phase0(c.into(), 1, &tables));
+
+    let c = Cube::solved();
+    let c = c.apply_move(Move(Face::F, 3));
+    let c = c.apply_move(Move(Face::R, 3));
+    assert!(!phase0(c.into(), 0, &tables));
+    assert!(!phase0(c.into(), 1, &tables));
+    assert!(phase0(c.into(), 2, &tables));
   }
 }
